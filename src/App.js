ebf469b8b56f4347930776785a3a122bae809ed9
Watson Chen <wc.watsonchen@gmail.com>
2022-10-07 15:33:42 -0400

    added routes, links and svg logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Navigation from "./routes/navigation/navigation.component"
 import Home from "./routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -30,7 +30,11 @@ const Home = () => {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
 };
 
 export default Home;
