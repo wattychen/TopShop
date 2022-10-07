169571957557d0cceb6f75ad4414fee60edeb12f
Watson Chen <wc.watsonchen@gmail.com>
2022-10-06 23:28:44 -0400

    setting up routes and home page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import "./categories.styles.scss";
-import Directory from "./components/directory/directory.component";
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl:
-        "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c25lYWtlcnN8ZW58MHx8MHx8&w=1000&q=80",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
 
+const App = () => {
   return (
-    // <div className="categories-container">
-    //   {categories.map((category) => (
-    //     <CategoryItem key={category.id} category={category} />
-    //   ))}
-    // </div>
-    <Directory categories={categories} />
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 };
 
