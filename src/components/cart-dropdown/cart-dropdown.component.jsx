7e628dfd1ba5b9b4a07a6da19028be1199b1ba82
Watson Chen <wc.watsonchen@gmail.com>
2022-10-15 15:31:50 -0400

    created method to add products to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,13 @@
 import Button from "../button/button.component";
+import CartItem from '../cart-item/cart-item.component';
 import "./cart-dropdown.styles.scss";
 
 const CartDropDown = () => {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {[].map(item => <CartItem cartItem={item} />)}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
