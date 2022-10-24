bd09198d9802a996329dbd716411501f6b1de87d
Watson Chen <wc.watsonchen@gmail.com>
2022-10-23 23:09:04 -0400

    updated routes for categories

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,41 @@ import "./directory.styles.jsx";
 import DirectoryItem from "../directory-item/directory-item.component";
 import { DirectoryContainer } from "./directory.styles.jsx";
 
-const Directory = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: 'shop/hats'
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: 'shop/jackets'
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c25lYWtlcnN8ZW58MHx8MHx8&w=1000&q=80",
+      route: 'shop/sneakers'
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: 'shop/womens'
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: 'shop/mens'
+  },
+];
+
+const Directory = () => {
   return (
     <DirectoryContainer className="directory-container">
       {categories.map((category) => (
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,38 +1,9 @@
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl:
-        "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c25lYWtlcnN8ZW58MHx8MHx8&w=1000&q=80",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <div>
-      <Directory categories={categories} />
+      <Directory />
     </div>
   );
 };
